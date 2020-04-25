8eeaf2d347cfb1272ebf30c2705e7b14aeaa25e5
Luke Rollins <lukerllns@gmail.com>
2020-04-25 00:02:57 -0400

    Erased some code and got the some of the constraints working. Also got a pattern for the phone numbers

--- form.js
+++ form.js
@@ -3,57 +3,24 @@
 const today = new Date();
 today.now()*/
 
-//let hangUp = document.getElementById('hangup-yes');
-//let hangOn = document.getElementById('hangup-no');
-
-/*function phoney() {
-    //let valUp = hangUp.value;
-    //console.log(valUp); 
-
-}
-// /^\d{7}(?:\d{3})?$/*/
-
-//let caller = document.getElementById("phoney");
 let named = document.getElementById("name");
 let namedError = document.querySelector("#name + span.error");
 let another = document.querySelector('input[value="from"]');
 let from = document.getElementById('from');
 let checker = document.getElementsByClassName("require-active");
-let show = document.querySelectorAll('[data-phone]');
-//let tell = noWay.getAttribute('data-reveal');
-//let x = document.getElementById("hook");
 let fudge = document.getElementById("hook");
+let brokenHeart = document.querySelector('#hook + span.error')
 let noWay = document.querySelector('input[value="hangup"]');
 let hangup = document.getElementById('hangup');
 let off = document.getElementById("you-not");
-let phoney = document.querySelector('#phoney');
-//let phoneyTwo = phoney.value;
+var phoney = document.getElementById('phoney');
 let phoneProb = document.querySelector('#phoney + span.error')
-//let ringu = document.getElementById('ringu');
-//let who =
-var tommy = new RegExp(/[\(\)]|\s|\-/g);
-//const ree = new RegExp(/^\d{7}(?:\d{3})?$/g);
-const ree = new RegExp(/^(\d{3})?(\d{3})(\d{4})$/);
+let offHookProb = document.querySelector('#you-not + span.error')
+const ree = new RegExp(/^(?:\(?([0-9]{3})\)?[-.*\s]?)?([0-9]{3})[-.*\s]?([0-9]{4})$/g);
 const meow = document.getElementById("meow");
 var lou = /502/;
 const forms = document.getElementsByTagName('form');
-let callMee = document.querySelectorAll('input[type="tel"]')
-
-
-//const chek = document.querySelectorAll('input[type="checkbox"]')
-
-/*function regPhone(e) {
-    e.preventDefault();
-
-}*/
-var phoneBlargh = () => {
-  var baloney = phoney.value.replace(tommy, "");
-  if (ree.test(baloney) != true) {
-    return;
-  }
-    
-  
-}
+let callMee = document.querySelectorAll('input[type="tel"]');
 
 for(let c = 0; c < callMee.length; c++) {
   callMee[c].setAttribute('pattern', ree.source)
@@ -67,105 +34,6 @@ for(let f = 0; f < forms.length; f++) {
 let erroneous = (field) => {
   if (from.checked != true || hangup.checked != true || field.type === '' ) return;
 }
-
-/*function validation() {
-  if(ree.test(phoney.value))
-  {
-    phoney.setCustomValidity(console.log('Yup'))
-  } else {
-    phoney.setCustomValidity(console.log('Nope'))
-  }
-}
-
-function validator() {
-  if(another.checked == true) {
-    if(ree.test(off.value)) {
-       off.setCustomValidity(console.log('Okiley dokily'))
-    } else {
-       off.setCustomValidity(console.log("D'oh"))
-    }
-  } else {
-     console.log("Don't have a cow")
-  }
-}*/
-/*var constraint = new RegExp(ree);
-console.log(constraint);
-phoney.addEventListener('input', validation)
-off.addEventListener('input', validator)
-function subMission(e) {
-  e.preventDefault();
-  let hooked = hangUp()
-  let junk = conjunc()
-  let train = conduct()
-  console.log(hooked);
-  console.log(named.value);  
-  console.log(junk);
-  console.log(train);  
-}*/
-
-/*function youI() {
-  addEventListener('click', conditional)
-  console.log('frack')
-}*/
-
-
-
-/*function conditional() {
-  //var bank = document.getElementsByClassName('require-active')
-
-show.forEach((checker) => {
-if (checker.checked == !true) {
-checker.setAttribute('required', 'required')}
-else {
-checker.removeAttribute('required')}})
-console.log("ugh")
-}*/
-
-//addEventListener('change', conditional)
-//addEventListener('click', bindUI)
-
-/*function conjunc() {
-  var baloney = phoney.value.replace(tommy, "");
-  if (ree.test(baloney) == true) {
-    if (lou.test(baloney) == true)
-    { return baloney.slice(3) && phoney.setCustomValidity(console.log('Yum'))}
-    else {
-      return baloney && phoney.setCustomValidity(console.log('Still yummy'))}
-  } 
-   else {
-     return "Oscar Meyer!" && phoney.setCustomValidity(console.log('Hungry'))
-   }
-      
-  }*/
-
-/*function conduct() {
-  if (lou.test(cheese) == true)
-    {return cheese.slice(3)
-    } else {
-      return cheese
-    } 
-}*/
-
-/*function conduct() {
-  var cheese = off.value.replace(tommy, "");
-  if (another.checked == true)
-  { if (ree.test(cheese) == true)
-    { if(lou.test(cheese) == true) {
-      return cheese.slice(3)
-    } else {return cheese}
-  }
-  } else {
-    return "Rats!!!"
-  }
-}
-
-function hangUp() {
-  if (fudge.hasAttribute('required')) {
-    return fudge.value
-  } else {
-    return "Nope!"
-  }
-}*/
 
 document.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -190,15 +58,67 @@ document.addEventListener('submit', (e) => {
   } else {
     phoneProb.innerHTML = '';
     phoneProb.className = 'error'
+    console.log(phoney.value + " called from;")
+     
   }
 })
 
+document.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if(another.checked == true && off.validity.valid) {
+    return console.log(off.value + " call back number")
+  } else {
+    return null
+  }
+})
+
+document.addEventListener('submit', (e) => {
+  e.preventDefault()
+    if(!off.validity.valid) {
+      phoneMalfunction() 
+    } else {
+      offHookProb.innerHTML = '';
+      offHookProb.className = 'error'
+      
+    }
+})
+
+
 function phoneyError() {
+  
   if (phoney.validity.valueMissing) {
     phoneProb.textContent = 'Need a number'
-  } else if (phoney.validity.patternMismatch)
-  { phoneProb.textContent = "That kinda number"}
+  } else if(phoney.validity.patternMismatch) {
+    phoneProb.textContent = "Not that number"
+  }
 }
+
+function phoneMalfunction() {
+  
+    if (off.validity.valueMissing) {
+      offHookProb.textContent = 'Need a number'  
+    } else if(off.validity.patternMismatch) {
+      offHookProb.textContent = "Not that number"
+    }
+  }
+
+function hungUp() {
+  if (fudge.validity.valueMissing) {
+    brokenHeart.textContent = "Check that 'Hang Up' box or enter something"
+  }
+}
+
+document.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if(!fudge.validity.valid) {
+    hungUp()
+  } else {
+    brokenHeart.innerHTML = '';
+    brokenHeart.className = 'error'
+  }
+})
+
+
 
 function ugh() {
     
@@ -222,21 +142,10 @@ function ugh() {
         } 
         }
     
-//console.log(baloney)
-//console.log(cheese)
-//meow.addEventListener('click', subMission);
-/*named.addEventListener('', () => {
-  if(named.validity.valid) {
-    namedError.innerHTML = '';
-    namedError.className = 'error'
-  } else {
-    namingError();
-  }
-})*/
+
 noWay.addEventListener('change', oogh);
 another.addEventListener('change', ugh);
-//document.addEventListener('input')
-//document.addEventListener('input', regPhone)
+
 
 /* var jenny = "812-867-5309";
 var tommy = new RegExp('[\(\)]|-', 'g');
@@ -336,6 +245,7 @@ conjunc()
 var regex = /\d{5}/;
 element.setAttribute("pattern", regex.source);
 */
+/*potential RegEx with and without area code ^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$ */
 
 
 
